d3d5232a582014cf2ba01e945bb07ac72ad3c7e1
Gary Soeller <gsoeller@eng.ucsd.edu>
2016-10-18 21:50:57 -0700

    AST refactor

--- ml/ast.ml
+++ ml/ast.ml
@@ -3,6 +3,55 @@
  *===----------------------------------------------------------------------===*)
 open Types
 
+type constantc_module = FDec of fdec list
+
+and fdec = FunctionDec of string * param list * constantc_type * stm list
+
+and arg = Arg of string * constantc_type
+
+and constantc_type =
+  | Int
+  | Bool
+  | String
+  | NoneType
+
+and param = { name: string; ty: constantc_type }
+
+and stm =
+  | VarDec of string * constantc_type * expr
+  | Assign of string * expr
+  | If of expr * stm list * stm list
+  | While of expr * stm list
+  | Return of expr
+
+and expr =
+  | Variable of string
+  | Unop of unop * expr
+  | Binop of binop * expr * expr
+  | Primitive of primitive
+  | CallExp of string * expr list
+
+and unop =
+  | B_Not
+  | Negate
+
+and binop =
+  | Plus
+  | Minus
+  | GT
+  | B_And
+
+and primitive = Number of int
+              | ByteArray of string
+              | Boolean of bool
+
+
+let ty_to_string = function
+  | Int -> "Int"
+  | Bool -> "Bool"
+  | String -> "String"
+
+(*********************************
 type expr =
   | Primitive of primitive
   | Variable of string
@@ -36,3 +85,4 @@ and primitive =
   | Bool of bool
 
 let _ = ()
+*)
