70d95601d3b197e753874bc902a9a72d9acf71b5
Gary Soeller <gsoeller@eng.ucsd.edu>
2016-10-31 13:59:51 -0700

    refactor codegen to not rely on state
    This change allows the compiler to iteratively build programs

--- test/test_codegen.ml
+++ test/test_codegen.ml
@@ -1,6 +1,24 @@
 open OUnit2
+open Ast
+open Codegen
+open Llvm
 
-let test1 ctx = assert_equal 1 1
+let prgm1 = FDec([FunctionDec("prgm1", [{name="x"; ty=Int}], Int, [
+  VarDec("y", Int, Primitive(Number 2));
+  Return (VarExp "y")])]);;
+
+let prgm2 = FDec([FunctionDec("prgm2", [{name="x"; ty=Int}], Int, [
+  VarDec("y", Int, Primitive(Number 2));
+  Return (VarExp "y")])]);;
+
+let ctx = global_context ()
+let m = create_module ctx "Test codegen"
+let a = (codegen ctx m prgm1);;
+let ctx' = global_context ()
+let m' = create_module ctx' "Test codegen"
+let b = (codegen ctx' m' prgm1);;
+
+let test1 ctx = assert_equal (string_of_llmodule m) (string_of_llmodule m')
 
 let suite =
   "codegen tests">:::
