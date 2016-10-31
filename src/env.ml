3b774c60056bf35599d3703e29b04ce1f92149b6
Gary Soeller <gsoeller@eng.ucsd.edu>
2016-10-31 14:45:08 -0700

    create a notion of a stdlib
    This creates a stdlib for constantc. There are two
    main parts: the "syntax" and the "semantics". The
    syntax includes the function names, types, and arg
     types. The semantics includes the LLVM IR. To reduce
    the complexity of generated IR, the codegen will only
    add stdlib functions if the callee function is unknown.

--- src/env.ml
+++ src/env.ml
@@ -1,3 +1,4 @@
+open Stdlib
 open Ast
 
 type ventry = { v_ty: constantc_type }
@@ -9,8 +10,9 @@ type entry =
 
 type env = (string,entry) Hashtbl.t
 
-(** Create the environment *)
 let venv =
   let v = Hashtbl.create 10 in
-  let _ = Hashtbl.add v "printf" (FunEntry { f_ty=Int; f_args=[ByteArr] }) in
+  let add_fun {name=n; ret_ty=ret; args_ty=args} =
+    Hashtbl.add v n (FunEntry {f_ty=ret; f_args=args}) in
+  let _ = List.map add_fun stdlib_funs in
   v
