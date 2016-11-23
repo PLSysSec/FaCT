a36cf9f1e24cd30341fc160a1777d1260870ab79
scauligi <scauligi@eng.ucsd.edu>
2016-11-23 13:15:37 -0800

    updated if transform

--- src/transform.ml
+++ src/transform.ml
@@ -1,6 +1,24 @@
 
 exception TransformError of string
 
+type context = Context of Cast.expr
+
+let ctx_expr (Context(e)) = e
+
+let new_temp_var =
+  let cntr = ref 0 in
+  let new_temp_var'() =
+    cntr := !cntr + 1;
+    "m" ^ (string_of_int !cntr)
+  in
+  new_temp_var'
+
+let b_true = Cast.Primitive(Cast.Number (-1))
+let b_false = Cast.Primitive(Cast.Number 0)
+let b_and l r = Cast.BinOp(Cast.BitAnd,l,r)
+let b_or l r = Cast.BinOp(Cast.BitOr,l,r)
+let b_not e = Cast.UnOp(Cast.BitNot,e)
+
 let rec transform = function
   | Ast.CModule fdecs ->
     let f = List.map transform_fdec fdecs in
@@ -14,33 +32,50 @@ and transform_type = function
 and transform_arg {Ast.name=n; Ast.ty=t} =
   {Cast.name=n; Cast.ty=transform_type(t)}
 
-and transform_stm_ctx ctx = function
-  | Ast.VarDec(n,ty,v) as vardec -> transform_stm vardec
-  | Ast.Assign(n,v) as a -> transform_stm a
-  | Ast.ArrAssign(n,i,v) as aa -> transform_stm aa
-  | Ast.If _ -> raise (TransformError "If transform not implemented")
-  | Ast.For(n,l,h,b) as f -> transform_stm f
-  | Ast.Return _ -> raise (TransformError "Return transform not implemented")
-
-and transform_stm = function
+and transform_stm ctx = function
   | Ast.VarDec(n,ty,v) ->
     let ty' = transform_type(ty) in
     let v' = transform_expr(v) in
     [Cast.VarDec(n,ty',v')]
-  | Ast.Assign(n,v) -> [Cast.Assign(n,transform_expr(v))]
-  | Ast.ArrAssign(n,i,v) -> [Cast.ArrAssign(n,i,(transform_expr v))]
-  | Ast.If _ -> raise (TransformError "If transform not implemented")
+  | Ast.Assign(n,v) ->
+    let c = ctx_expr ctx in
+    let v' = transform_expr(v) in
+    let assign_ok = b_and c (b_not (Cast.VarExp "rset")) in
+    let newval = b_and v' assign_ok in
+    let oldval = b_and (Cast.VarExp n) (b_not assign_ok) in
+    [Cast.Assign(n,(b_or newval oldval))]
+  | Ast.ArrAssign(n,i,v) ->
+    let c = ctx_expr ctx in
+    let v' = transform_expr(v) in
+    let assign_ok = b_and c (Cast.VarExp "rset") in
+    let newval = b_and v' assign_ok in
+    let oldval = b_and (Cast.ArrExp(n,i)) (b_not assign_ok) in
+    [Cast.ArrAssign(n,i,(b_or newval oldval))]
+  | Ast.If(e,bt,bf) ->
+    let c = ctx_expr ctx in
+    let e' = transform_expr(e) in
+    let tname = new_temp_var() in
+    let m = Cast.VarExp tname in
+    let c' = b_and m c in
+    let ctx' = Context(c') in
+    let bt' = List.flatten(List.map (transform_stm ctx') bt) in
+    let bf' = List.flatten(List.map (transform_stm ctx') bf) in
+    let mdec = Cast.VarDec(tname,Cast.Int,b_and e' c) in
+    let mnot = Cast.Assign(tname,b_not m) in
+    [mdec] @ bt' @ [mnot] @ bf'
   | Ast.For(n,l,h,b) ->
     let l' = transform_primitive l in
     let h' = transform_primitive h in
-    let b' = List.flatten(List.map transform_stm b) in
+    let b' = List.flatten(List.map (transform_stm ctx) b) in
     [Cast.For(n,l',h',b')]
   | Ast.Return e ->
-    let rset = Cast.Primitive(Cast.Number (Int32.to_int Int32.max_int)) in
-    let bnot = Cast.UnOp(Cast.BitNot,Cast.VarExp "rset") in
-    let bitand = Cast.BinOp(Cast.BitAnd, transform_expr e, bnot) in
-    let bitor = Cast.BinOp(Cast.BitOr,Cast.VarExp("rval"),bitand) in
-    [Cast.Assign("rval", bitor); Cast.Assign("rset",rset)]
+    let c = ctx_expr ctx in
+    let e' = transform_expr(e) in
+    let rval = Cast.VarExp "rval" in
+    let rset = Cast.VarExp "rset" in
+    let assign_ok = b_and c (b_not rset) in
+    let newval = b_and e' assign_ok in
+    [Cast.Assign("rval",(b_or rval newval)); Cast.Assign("rset",(b_or rset c))]
 
 and transform_expr = function
   | Ast.VarExp s -> Cast.VarExp s
@@ -58,7 +93,7 @@ and transform_expr = function
 
 and transform_primitive = function
   | Ast.Number n -> Cast.Number n
-  | Ast.Boolean true -> Cast.Number (Int32.to_int Int32.max_int)
+  | Ast.Boolean true -> Cast.Number (-1)
   | Ast.Boolean false -> Cast.Number 0
   | Ast.ByteArray s -> Cast.ByteArray s
 
@@ -76,7 +111,8 @@ and transform_fdec = function
   | Ast.FunctionDec(name,args,rt,body) ->
     let args' = List.map transform_arg args in
     let rt' = transform_type(rt) in
-    let body' = List.flatten(List.map transform_stm body) in
+    let ctx = Context(Cast.Primitive(Cast.Number (-1))) in
+    let body' = List.flatten(List.map (transform_stm ctx) body) in
     let rval = Cast.VarDec("rval",Cast.Int,Cast.Primitive(Cast.Number 0)) in
     let rset = Cast.VarDec("rset",Cast.Int,Cast.Primitive(Cast.Number 0)) in
     let body'' = [rval]@[rset]@body' in
