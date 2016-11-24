27fc4bba113185fc0e4c57ed5d9d9a683798ae59
Gary Soeller <gsoeller@eng.ucsd.edu>
2016-11-23 18:03:03 -0800

    add tests for if statements

--- harness.c
+++ harness.c
@@ -4,10 +4,14 @@
 int get100();
 int getByteArrIndex();
 int setByteArrIndex();
-int mutateArray();
-int mutateArray2();
-int identity();
-int simpleIf();
+int mutateArray(int*);
+int mutateArray2(int*,int*);
+int identity(int*);
+int simpleIf(int*);
+int mediumComplexIf(int*);
+int mixedIf(int*);
+int mixedIf2(int*);
+int nestedIf(int*);
 
 void test_get(void) {
   TEST_ASSERT_EQUAL(100, get100());
@@ -48,6 +52,48 @@ void test_simple_if(void) {
   TEST_ASSERT_EQUAL(2,simpleIf(eleven));
 }
 
+void test_medium_complex_if(void) {
+  int *one = malloc(sizeof(int));
+  *one = 1;
+  int *eleven = malloc(sizeof(int));
+  *eleven = 11;
+  TEST_ASSERT_EQUAL(1,mediumComplexIf(one));
+  TEST_ASSERT_EQUAL(2,mediumComplexIf(eleven));
+}
+
+void test_mixed_if(void) {
+  int *one = malloc(sizeof(int));
+  *one = 1;
+  int *eleven = malloc(sizeof(int));
+  *eleven = 11;
+  TEST_ASSERT_EQUAL(1,mixedIf(one));
+  TEST_ASSERT_EQUAL(2,mixedIf(eleven));
+}
+
+void test_mixed_if2(void) {
+  int *one = malloc(sizeof(int));
+  *one = 1;
+  int *eleven = malloc(sizeof(int));
+  *eleven = 11;
+  TEST_ASSERT_EQUAL(1,mixedIf2(one));
+  TEST_ASSERT_EQUAL(2,mixedIf2(eleven));
+}
+
+void test_nested_if(void) {
+  int *one = malloc(sizeof(int));
+  *one = 1;
+  int *seven = malloc(sizeof(int));
+  *seven = 7;
+  int *eleven = malloc(sizeof(int));
+  *eleven = 11;
+  int *sixteen = malloc(sizeof(int));
+  *sixteen = 16;
+  TEST_ASSERT_EQUAL(1,nestedIf(one));
+  TEST_ASSERT_EQUAL(2,nestedIf(seven));
+  TEST_ASSERT_EQUAL(3,nestedIf(eleven));
+  TEST_ASSERT_EQUAL(4,nestedIf(sixteen));
+}
+
 int main(void) {
   UNITY_BEGIN();
   RUN_TEST(test_get);
@@ -57,5 +103,9 @@ int main(void) {
   RUN_TEST(test_mutate_array2);
   RUN_TEST(test_identity);
   RUN_TEST(test_simple_if);
+  RUN_TEST(test_medium_complex_if);
+  RUN_TEST(test_mixed_if);
+  RUN_TEST(test_mixed_if2);
+  RUN_TEST(test_nested_if);
   return UNITY_END();
 }
--- test/test_semant_driver.ml
+++ test/test_semant_driver.ml
@@ -42,9 +42,47 @@ let else' = Return(Primitive(Number 2))
 let if' = If(cond,[then'],[else'])
 let prgm7 = FunctionDec("simpleIf",[if_arg],Int,[if'])
 
+(* Medium Complex If *)
+let if_dec = VarDec("complex_ret", Int, Primitive(Number 10))
+let then'' = Assign("complex_ret", Primitive(Number 1))
+let else'' = Assign("complex_ret", Primitive(Number 2))
+let if'' = If(cond,[then''],[else''])
+let ret''' = Return(VarExp "complex_ret")
+let prgm8 = FunctionDec("mediumComplexIf", [if_arg], Int, [if_dec;if'';ret'''])
+
+(* Mixed If *)
+let if''' = If(cond,[then'],[else''])
+let prgm9 = FunctionDec("mixedIf", [if_arg], Int, [if_dec;if''';ret'''])
+
+(* Mixed If 2 *)
+let if4 = If(cond,[then''],[else'])
+let prgm10 = FunctionDec("mixedIf2", [if_arg], Int, [if_dec;if4;ret'''])
+
+(* Nested If *)
+let cond_a = BinOp(GT, Primitive(Number 5), VarExp("cond"))
+let cond_b = BinOp(GT, Primitive(Number 15), VarExp("cond"))
+let ret1 = Return(Primitive(Number 1))
+let ret2 = Return(Primitive(Number 2))
+let ret3 = Return(Primitive(Number 3))
+let ret4 = Return(Primitive(Number 4))
+let if_a = If(cond_a,[ret1],[ret2])
+let if_b = If(cond_b,[ret3],[ret4])
+let nif = If(cond,[if_a],[if_b])
+let prgm11 = FunctionDec("nestedIf", [if_arg], Int, [nif])
 
 (* Module 1 *)
-let m1 = CModule [prgm1;prgm2;prgm3;prgm4;prgm5;prgm6;prgm7]
+let m1 = CModule
+    [prgm1;
+     prgm2;
+     prgm3;
+     prgm4;
+     prgm5;
+     prgm6;
+     prgm7;
+     prgm8;
+     prgm9;
+     prgm10;
+     prgm11]
 
 (* List of modules to test *)
 let programs = [m1]
