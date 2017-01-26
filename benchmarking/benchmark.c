2ab050eaace62e4bd5cf2826d0454cd0cfe887ba
Brian Johannesmeyer <bjohanne@eng.ucsd.edu>
2017-01-26 13:05:28 -0800

    Added makefile and separate stats library

--- benchmarking/benchmark.c
+++ benchmarking/benchmark.c
@@ -3,7 +3,7 @@
 #include <inttypes.h>
 #include <string.h>
 #include <stdlib.h>
-#include <math.h>
+#include "stats.h"
 
 #define NUM_TRIALS 512
 #define NUM_ITRS 512
@@ -89,57 +89,11 @@ runme:
 	return (int64_t)(routine_time - offset_time) / (double) NUM_ITRS;
 }
 
-/*
-NO_OPT
-void warmup_dcache(void * buf) { 
-	int i;
-	for(i = 0; i < WARMUP_COUNT; i++) {
-		memset(buf, 0x34, NBYTES);
-		barrier_data(buf);
-	}
-}
-*/
-
 void print_times(const double * times, int n) {
 	int i;
 	
 	for(i = 0; i < n; i++)
 		printf("%.9f\n", times[i]);
-}
-
-int compare_double(const void * aptr, const void * bptr) {
-	double a = *(const double*)aptr;
-	double b = *(const double*)bptr;
-	
-	if (a < b)
-		return -1;
-	else if (a > b)
-		return 1;
-	else
-		return 0;
-}
-
-double get_median(const double * data, int n) {
-	double * datacopy;
-	
-	datacopy = malloc(n * sizeof(double));
-	memcpy(datacopy, data, n * sizeof(double));
-	qsort(datacopy, n, sizeof(double), compare_double);
-	return datacopy[n/2];
-}
-
-double get_mean(const double * data, int n) {
-	double sum=0;
-	int i;
-	for(i = 0; i < n; ++i) sum+=data[i];
-	return sum / n;
-}
-
-double get_standard_deviation(const double * data, int n, double mean) {
-	double sum_deviation = 0;
-	int i;
-	for(i = 0; i < n; ++i) sum_deviation+=(data[i]-mean)*(data[i]-mean);
-	return sqrt(sum_deviation / n);
 }
 
 NO_OPT
@@ -150,7 +104,6 @@ int main(int argc, char* argv[]) {
 
         //setup()        
 
-	//warmup_dcache(buf);
 	for(i = 0; i < NUM_TRIALS; i++)
 		times[i] = test();
 
