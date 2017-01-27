8a9f5254537250328ac616a6b513cd8705a0a09d
Brian Johannesmeyer <bjohanne@eng.ucsd.edu>
2017-01-27 14:08:40 -0800

    Took out fxn declaration, now just inlining initialization

--- benchmarking/projects/sort/sort.h
+++ benchmarking/projects/sort/sort.h
@@ -6,12 +6,10 @@
     int out[3]; \
     int in[3]; 
 
-#define ROUTINE_INIT reinit(conds,out,in);
-void reinit(int *conds,int *out,int *in) {
-    memcpy(conds, (int [3]){0,0,0}, 3*sizeof(int));
-    memcpy(out, (int [3]){0,0,0}, 3*sizeof(int));
+#define ROUTINE_INIT \
+    memcpy(conds, (int [3]){0,0,0}, 3*sizeof(int)); \
+    memcpy(out, (int [3]){0,0,0}, 3*sizeof(int)); \
     memcpy(in, (int [3]){6,1,3}, 3*sizeof(int));
-}
 
 #define ROUTINE sort3(conds,out,in);
 void sort3(int *conds, int *out, int *in);
