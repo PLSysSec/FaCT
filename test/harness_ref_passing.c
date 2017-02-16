e8d0c34ddd4ebf1af9cb294115a966b0638d7a48
scauligi <scauligi@eng.ucsd.edu>
2017-02-15 17:14:52 -0800

    Functions pass context through calls
    Trello card https://trello.com/c/6h3c15Co/56

--- test/harness_ref_passing.c
+++ test/harness_ref_passing.c
@@ -4,6 +4,8 @@
 
 int32_t mutateRef(uint8_t* p);
 int32_t passAlong(uint8_t* p);
+int32_t mutateIf5(int32_t cond, uint8_t* p);
+int32_t mutateIfNot5(int32_t cond, uint8_t* p);
 
 void test_mutateref() {
   uint8_t c = 0;
@@ -15,4 +17,21 @@ void test_passalong() {
   uint8_t c = 0;
   passAlong(&c);
   TEST_ASSERT_EQUAL(22, c);
+}
+
+void test_mutate_if() {
+  uint8_t c = 0;
+  mutateIf5(4, &c);
+  TEST_ASSERT_EQUAL(0, c);
+  mutateIf5(5, &c);
+  TEST_ASSERT_EQUAL(22, c);
+}
+
+void test_mutate_if_swapped() {
+  uint8_t c = 0;
+  mutateIfNot5(4, &c);
+  TEST_ASSERT_EQUAL(22, c);
+  c = 0;
+  mutateIfNot5(5, &c);
+  TEST_ASSERT_EQUAL(0, c);
 }
