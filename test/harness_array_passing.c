1cdf1b9fcc34a5bbfd55aa6914529d6c11d73913
scauligi <scauligi@eng.ucsd.edu>
2017-02-17 13:30:13 -0800

    additional tests for function context passing

--- test/harness_array_passing.c
+++ test/harness_array_passing.c
@@ -5,6 +5,8 @@
 int32_t mutateArray(uint8_t arr[4]);
 int32_t passAlong(uint8_t arr[4]);
 int32_t copyArray(uint8_t arr[4]);
+int32_t mutateIf5(uint8_t arr[4], int32_t cond);
+int32_t mutateIfNot5(uint8_t arr[4], int32_t cond);
 
 void test_mutatearray() {
   uint8_t arr[4] = {2, 3, 4, 5};
@@ -31,4 +33,26 @@ void test_copyarray() {
   TEST_ASSERT_EQUAL(22, arr[1]);
   TEST_ASSERT_EQUAL(22, arr[2]);
   TEST_ASSERT_EQUAL(22, arr[3]);
+}
+
+void test_mutate_if() {
+  uint8_t arr[4] = {2, 3, 4, 5};
+  mutateIf5(arr, 6);
+  for (int i = 0; i < 4; i++)
+    TEST_ASSERT_EQUAL(i + 2, arr[i]);
+  mutateIf5(arr, 5);
+  for (int i = 0; i < 4; i++)
+    TEST_ASSERT_EQUAL(22, arr[i]);
+}
+
+void test_mutate_if_swapped() {
+  uint8_t arr[4] = {2, 3, 4, 5};
+  mutateIfNot5(arr, 6);
+  for (int i = 0; i < 4; i++) {
+    TEST_ASSERT_EQUAL(22, arr[i]);
+    arr[i] = i + 2;
+  }
+  mutateIfNot5(arr, 5);
+  for (int i = 0; i < 4; i++)
+    TEST_ASSERT_EQUAL(i + 2, arr[i]);
 }
