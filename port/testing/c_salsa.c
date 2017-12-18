d8a175e57ca2150af01db44e9d6945ba5169d4b9
scauligi <scauligi@eng.ucsd.edu>
2017-12-17 18:03:47 -0800

    bugfixes; almost almost done with cryptobox port

--- port/testing/c_salsa.c
+++ port/testing/c_salsa.c
@@ -1,5 +1,11 @@
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
+
+void sodium_memzero(void* buf, size_t len)
+{
+    memset(buf, 0, len);
+}
 
 #define LOAD32_LE(SRC) load32_le(SRC)
 static inline uint32_t
@@ -38,6 +44,9 @@ rotl32(const uint32_t x, const int b)
 {
     return (x << b) | (x >> (32 - b));
 }
+
+#define ROUNDS 20
+#define U32C(v) (v##U)
 
 void
 c_crypto_core_salsa(unsigned char *out, const unsigned char *in,
@@ -124,4 +133,136 @@ c_crypto_core_salsa(unsigned char *out, const unsigned char *in,
     STORE32_LE(out + 52, x13 + j13);
     STORE32_LE(out + 56, x14 + j14);
     STORE32_LE(out + 60, x15 + j15);
+}
+
+int
+c_crypto_core_hsalsa20(unsigned char *out,
+                     const unsigned char *in,
+                     const unsigned char *k,
+                     const unsigned char *c)
+{
+    uint32_t x0, x1, x2, x3, x4, x5, x6, x7, x8,
+             x9, x10, x11, x12, x13, x14,  x15;
+    int      i;
+
+    if (c == NULL) {
+        x0 = U32C(0x61707865);
+        x5 = U32C(0x3320646e);
+        x10 = U32C(0x79622d32);
+        x15 = U32C(0x6b206574);
+    } else {
+        x0 = LOAD32_LE(c + 0);
+        x5 = LOAD32_LE(c + 4);
+        x10 = LOAD32_LE(c + 8);
+        x15 = LOAD32_LE(c + 12);
+    }
+    x1 = LOAD32_LE(k + 0);
+    x2 = LOAD32_LE(k + 4);
+    x3 = LOAD32_LE(k + 8);
+    x4 = LOAD32_LE(k + 12);
+    x11 = LOAD32_LE(k + 16);
+    x12 = LOAD32_LE(k + 20);
+    x13 = LOAD32_LE(k + 24);
+    x14 = LOAD32_LE(k + 28);
+    x6 = LOAD32_LE(in + 0);
+    x7 = LOAD32_LE(in + 4);
+    x8 = LOAD32_LE(in + 8);
+    x9 = LOAD32_LE(in + 12);
+
+    for (i = ROUNDS; i > 0; i -= 2) {
+        x4 ^= ROTL32(x0 + x12, 7);
+        x8 ^= ROTL32(x4 + x0, 9);
+        x12 ^= ROTL32(x8 + x4, 13);
+        x0 ^= ROTL32(x12 + x8, 18);
+        x9 ^= ROTL32(x5 + x1, 7);
+        x13 ^= ROTL32(x9 + x5, 9);
+        x1 ^= ROTL32(x13 + x9, 13);
+        x5 ^= ROTL32(x1 + x13, 18);
+        x14 ^= ROTL32(x10 + x6, 7);
+        x2 ^= ROTL32(x14 + x10, 9);
+        x6 ^= ROTL32(x2 + x14, 13);
+        x10 ^= ROTL32(x6 + x2, 18);
+        x3 ^= ROTL32(x15 + x11, 7);
+        x7 ^= ROTL32(x3 + x15, 9);
+        x11 ^= ROTL32(x7 + x3, 13);
+        x15 ^= ROTL32(x11 + x7, 18);
+        x1 ^= ROTL32(x0 + x3, 7);
+        x2 ^= ROTL32(x1 + x0, 9);
+        x3 ^= ROTL32(x2 + x1, 13);
+        x0 ^= ROTL32(x3 + x2, 18);
+        x6 ^= ROTL32(x5 + x4, 7);
+        x7 ^= ROTL32(x6 + x5, 9);
+        x4 ^= ROTL32(x7 + x6, 13);
+        x5 ^= ROTL32(x4 + x7, 18);
+        x11 ^= ROTL32(x10 + x9, 7);
+        x8 ^= ROTL32(x11 + x10, 9);
+        x9 ^= ROTL32(x8 + x11, 13);
+        x10 ^= ROTL32(x9 + x8, 18);
+        x12 ^= ROTL32(x15 + x14, 7);
+        x13 ^= ROTL32(x12 + x15, 9);
+        x14 ^= ROTL32(x13 + x12, 13);
+        x15 ^= ROTL32(x14 + x13, 18);
+    }
+
+    STORE32_LE(out + 0, x0);
+    STORE32_LE(out + 4, x5);
+    STORE32_LE(out + 8, x10);
+    STORE32_LE(out + 12, x15);
+    STORE32_LE(out + 16, x6);
+    STORE32_LE(out + 20, x7);
+    STORE32_LE(out + 24, x8);
+    STORE32_LE(out + 28, x9);
+
+    return 0;
+}
+
+int
+c_stream_ref_xor_ic(unsigned char *c, const unsigned char *m,
+                  unsigned long long mlen, const unsigned char *n, uint64_t ic,
+                  const unsigned char *k)
+{
+    unsigned char in[16];
+    unsigned char block[64];
+    unsigned char kcopy[32];
+    unsigned int  i;
+    unsigned int  u;
+
+    if (!mlen) {
+        return 0;
+    }
+    for (i = 0; i < 32; i++) {
+        kcopy[i] = k[i];
+    }
+    for (i = 0; i < 8; i++) {
+        in[i] = n[i];
+    }
+    for (i = 8; i < 16; i++) {
+        in[i] = (unsigned char) (ic & 0xff);
+        ic >>= 8;
+    }
+    while (mlen >= 64) {
+        c_crypto_core_salsa(block, in, kcopy, NULL, 20);
+        for (i = 0; i < 64; i++) {
+            c[i] = m[i] ^ block[i];
+        }
+        u = 1;
+        for (i = 8; i < 16; i++) {
+            u += (unsigned int) in[i];
+            in[i] = u;
+            u >>= 8;
+        }
+        mlen -= 64;
+        c += 64;
+        m += 64;
+    }
+    if (mlen) {
+        c_crypto_core_salsa(block, in, kcopy, NULL, 20);
+        for (i = 0; i < (unsigned int) mlen; i++) {
+            c[i] = m[i] ^ block[i];
+        }
+    }
+    sodium_memzero(block, sizeof block);
+    sodium_memzero(kcopy, sizeof kcopy);
+
+    return 0;
 }
