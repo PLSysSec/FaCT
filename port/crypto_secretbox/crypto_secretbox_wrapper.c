a60cb4837327032d22020875dc310409831ef1f1
scauligi <scauligi@eng.ucsd.edu>
2017-12-18 01:44:11 -0800

    added test for secretbox_open (and it passes!)

--- port/crypto_secretbox/crypto_secretbox_wrapper.c
+++ port/crypto_secretbox/crypto_secretbox_wrapper.c
@@ -5,7 +5,16 @@ int32_t crypto_secretbox(
     uint8_t c[],
     const uint8_t m[],
     uint32_t __m_len,
-    const uint8_t n[16],
+    const uint8_t n[24],
     const uint8_t k[32]) {
   return _crypto_secretbox(c, __m_len, m, __m_len, n, k, 1);
+}
+
+int32_t crypto_secretbox_open(
+    uint8_t m[],
+    const uint8_t c[],
+    uint32_t __c_len,
+    const uint8_t n[24],
+    const uint8_t k[32]) {
+  return _crypto_secretbox_open(m, __c_len, c, __c_len, n, k, 1);
 }
