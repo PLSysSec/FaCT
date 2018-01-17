fca67384b11bdc97fb3a5b6379deb74100baac07
scauligi <scauligi@eng.ucsd.edu>
2018-01-17 04:34:06 -0800

    fixed tests

--- test/harness_array_passing.c
+++ test/harness_array_passing.c
@@ -5,7 +5,7 @@
 
 void test_mutatearray() {
   uint8_t arr[4] = {2, 3, 4, 5};
-  mutateArray(arr,1);
+  mutateArray(arr);
   TEST_ASSERT_EQUAL(22, arr[0]);
   TEST_ASSERT_EQUAL(22, arr[1]);
   TEST_ASSERT_EQUAL(22, arr[2]);
@@ -14,7 +14,7 @@ void test_mutatearray() {
 
 void test_passalong() {
   uint8_t arr[4] = {2, 3, 4, 5};
-  passAlong(arr,1);
+  passAlong(arr);
   TEST_ASSERT_EQUAL(22, arr[0]);
   TEST_ASSERT_EQUAL(22, arr[1]);
   TEST_ASSERT_EQUAL(22, arr[2]);
@@ -23,7 +23,7 @@ void test_passalong() {
 
 void test_copyarray() {
   uint8_t arr[4] = {2, 3, 4, 5};
-  copyArray(arr,1);
+  copyArray(arr);
   TEST_ASSERT_EQUAL(22, arr[0]);
   TEST_ASSERT_EQUAL(22, arr[1]);
   TEST_ASSERT_EQUAL(22, arr[2]);
@@ -37,7 +37,7 @@ void test_dynamicarray() {
     arr[i] = i;
   }
 
-  dynamicArray(arr, 7, 1);
+  dynamicArray(arr, 7);
 
   for (uint32_t i = 0; i < 7; i++) {
     TEST_ASSERT_EQUAL(i + 1, arr[i]);
@@ -46,22 +46,22 @@ void test_dynamicarray() {
 
 void test_mutate_if() {
   uint8_t arr[4] = {2, 3, 4, 5};
-  mutateIf5(arr, 6, 1);
+  mutateIf5(arr, 6);
   for (int i = 0; i < 4; i++)
     TEST_ASSERT_EQUAL(i + 2, arr[i]);
-  mutateIf5(arr, 5, 1);
+  mutateIf5(arr, 5);
   for (int i = 0; i < 4; i++)
     TEST_ASSERT_EQUAL(22, arr[i]);
 }
 
 void test_mutate_if_swapped() {
   uint8_t arr[4] = {2, 3, 4, 5};
-  mutateIfNot5(arr, 6, 1);
+  mutateIfNot5(arr, 6);
   for (int i = 0; i < 4; i++) {
     TEST_ASSERT_EQUAL(22, arr[i]);
     arr[i] = i + 2;
   }
-  mutateIfNot5(arr, 5, 1);
+  mutateIfNot5(arr, 5);
   for (int i = 0; i < 4; i++)
     TEST_ASSERT_EQUAL(i + 2, arr[i]);
 }
