81b4f81db154e4c14049ee955e0609ce91dd0b1d
scauligi <scauligi@eng.ucsd.edu>
2018-02-15 14:34:46 -0800

    64 bit versions of stdlibs

--- src/stdlib.cppo.ml
+++ src/stdlib.cppo.ml
@@ -8,9 +8,8 @@ open Llvm
 let p : pos = {file=""; line=0; lpos=0; rpos=0}
 #define mkpos make_ast p @@
 
-let load_le_proto () =
-  let n = 32 in
-  let name = mkpos "_load_le" in
+let load_le_proto' n name' =
+  let name = mkpos name' in
   let ft = { export=false; inline=Always } in
 
   let rt' = mkpos BaseET(mkpos UInt n, mkpos Fixed Secret) in
@@ -21,11 +20,15 @@ let load_le_proto () =
   let params = [mkpos Param (mkpos "src", arg)] in
 
   let fdec = mkpos (StdlibFunDec(name,ft,rt,params)) in
-  name,fdec
+    name,fdec
 
-let store_le_proto () =
-  let n = 32 in
-  let name = mkpos "_store_le" in
+let load_le_proto () =
+  load_le_proto' 32 "_load_le"
+let load64_le_proto () =
+  load_le_proto' 64 "_load64_le"
+
+let store_le_proto' n name' =
+  let name = mkpos name' in
   let ft = { export=false; inline=Always } in
   let rt = None in
 
@@ -36,21 +39,29 @@ let store_le_proto () =
   let params = [mkpos Param (mkpos "dst", arg); mkpos Param (mkpos "w", w)] in
 
   let fdec = mkpos (StdlibFunDec(name,ft,rt,params)) in
-  name,fdec
+    name,fdec
 
-let memzero_proto () =
-  let name = mkpos "_memzero" in
+let store_le_proto () =
+  store_le_proto' 32 "_store_le"
+let store64_le_proto () =
+  store_le_proto' 64 "_store64_le"
+
+let memzero_proto' n name' () =
+  let name = mkpos name' in
   let ft = { export=false; inline=Never } in
   let rt = None in
 
-  let arr = mkpos ArrayAT(mkpos UInt 8, mkpos LDynamic (mkpos "_len")) in
+  let arr = mkpos ArrayAT(mkpos UInt n, mkpos LDynamic (mkpos "_len")) in
   let arg = mkpos ArrayVT(arr, mkpos Fixed Secret, mkpos Mut) in
 
   let len = mkpos RefVT(mkpos UInt 32, mkpos Fixed Public, mkpos Const) in
   let params = [mkpos Param (mkpos "arr", arg); mkpos Param (mkpos "_len", len)] in
 
   let fdec = mkpos (StdlibFunDec(name,ft,rt,params)) in
-  name,fdec
+    name,fdec
+
+let memzero_proto = memzero_proto' 8 "_memzero"
+let memzero64_proto = memzero_proto' 64 "_memzero64"
 
 let arrcopy_proto () =
   let name = mkpos "_arrcopy" in
@@ -66,14 +77,13 @@ let arrcopy_proto () =
   let fdec = mkpos (StdlibFunDec(name,ft,rt,params)) in
   name,fdec
 
-let load_le_codegen llcontext llmodule =
-  let n = 32 in
+let load_le_codegen' n name llcontext llmodule =
   let ity = integer_type llcontext n in
   let bty = i8_type llcontext in
   let aty = pointer_type bty in
   let pty = pointer_type ity in
   let ft = function_type ity [| aty |] in
-  let fn = declare_function "_load_le" ft llmodule in
+  let fn = declare_function name ft llmodule in
     add_function_attr fn Alwaysinline;
     add_function_attr fn Readonly;
     set_linkage Internal fn;
@@ -88,14 +98,18 @@ let load_le_codegen llcontext llmodule =
       build_ret load b;
       fn
 
-let store_le_codegen llcontext llmodule =
-  let n = 32 in
+let load_le_codegen =
+  load_le_codegen' 32 "_load_le"
+let load64_le_codegen =
+  load_le_codegen' 64 "_load64_le"
+
+let store_le_codegen' n name llcontext llmodule =
   let ity = integer_type llcontext n in
   let bty = i8_type llcontext in
   let aty = pointer_type bty in
   let pty = pointer_type ity in
   let ft = function_type (void_type llcontext) [| aty; ity |] in
-  let fn = declare_function "_store_le" ft llmodule in
+  let fn = declare_function name ft llmodule in
     add_function_attr fn Alwaysinline;
     set_linkage Internal fn;
   let bb = append_block llcontext "entry" fn in
@@ -108,7 +122,12 @@ let store_le_codegen llcontext llmodule =
       build_ret_void b;
       fn
 
-let memzero_codegen llcontext llmodule =
+let store_le_codegen =
+  store_le_codegen' 32 "_store_le"
+let store64_le_codegen =
+  store_le_codegen' 64 "_store64_le"
+
+let memzero_codegen' n name llcontext llmodule =
   let i8_ty = i8_type llcontext in
   let i32_ty = i32_type llcontext in
   let ptr_ty = pointer_type i8_ty in
@@ -116,12 +135,14 @@ let memzero_codegen llcontext llmodule =
   let arg_types = [| ptr_ty; i8_ty; i32_ty; i32_ty; bool_ty |] in
   let vt = void_type llcontext in
   let ft = function_type vt arg_types in
-  let memset = declare_function "llvm.memset.p0i8.i32" ft llmodule in
+  let memset = declare_function ("llvm.memset.p0i8.i32") ft llmodule in
 
-  let arg_types = [| ptr_ty; i32_ty; |] in
+  let ity = integer_type llcontext n in
+  let iptr_ty = pointer_type ity in
+  let arg_types = [| iptr_ty; i32_ty; |] in
   let vt = void_type llcontext in
   let ft = function_type vt arg_types in
-  let fn = declare_function "_memzero" ft llmodule in
+  let fn = declare_function name ft llmodule in
     add_function_attr fn Noinline;
     set_linkage Internal fn;
   let bb = append_block llcontext "entry" fn in
@@ -130,21 +151,31 @@ let memzero_codegen llcontext llmodule =
     let arr = param fn 0 in
     let len = param fn 1 in
     let zero = const_int i8_ty 0 in
-    let alignment = const_int i32_ty 4 in
+    let alignment = const_int i32_ty (n / 8) in
+    let arr' = build_bitcast arr ptr_ty "_secret_cast" b in
     let volatility = const_int bool_ty 0 in
-    let args = [| arr; zero; len; alignment; volatility |] in
+    let args = [| arr'; zero; len; alignment; volatility |] in
       build_call memset args "" b;
       build_ret_void b;
       fn
 
+let memzero_codegen = memzero_codegen' 8 "_memzero"
+let memzero64_codegen = memzero_codegen' 64 "_memzero64"
+
 let get_stdlib name llctx llmod =
   match name with
     | "_load_le" -> load_le_codegen llctx llmod
+    | "_load64_le" -> load64_le_codegen llctx llmod
     | "_store_le" -> store_le_codegen llctx llmod
+    | "_store64_le" -> store64_le_codegen llctx llmod
     | "_memzero" -> memzero_codegen llctx llmod
+    | "_memzero64" -> memzero64_codegen llctx llmod
 
 let functions = [
   load_le_proto ();
+  load64_le_proto ();
   store_le_proto ();
+  store64_le_proto ();
   memzero_proto ();
+  memzero64_proto ();
 ]
