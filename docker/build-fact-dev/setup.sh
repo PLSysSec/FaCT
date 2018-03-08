1a9b3acdea5a2ecf6929a33e65778b8cd2644471
Brian Johannesmeyer <bjohanne@eng.ucsd.edu>
2018-03-07 18:43:46 -0800

    docker renaming, and fact dev

---
+++ docker/build-fact-dev/setup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash 
+
+docker build -t fact_dev .
--- docker/run.sh
+++ docker/run.sh
@@ -4,5 +4,5 @@ docker run -it --rm \
     -v $(pwd)/..:/home/docker/FaCT \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -e HOST_USER_ID=$(id -u) -e HOST_USER_GID=$(id -g) \
-    bjohannesmeyer/fact /bin/bash
+    bjohannesmeyer/fact_dev /bin/bash
 
