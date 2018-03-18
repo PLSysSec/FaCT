6c6ac6029b277963dd42fa82e1338bde5f44dad1
Gary Soeller <gsoeller@ucsd.edu>
2018-03-17 19:35:03 -0700

    no public out

--- docker/build-ctverif/wrapperW.c
+++ docker/build-ctverif/wrapperW.c
@@ -7,6 +7,6 @@ uint32_t sum_array(uint8_t *arr, uint32_t arr_len);
 void wrapper(uint8_t *arr, uint32_t arr_len) {
         public_in(__SMACK_value(arr));
         public_in(__SMACK_value(arr_len));
-        public_out(__SMACK_return_value());
+        //public_out(__SMACK_return_value());
         sum_array(arr,arr_len);
 }
