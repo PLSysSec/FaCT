57446de36e0cdd4d4ad40490de129d1a04e7eb60
scauligi <scauligi@eng.ucsd.edu>
2018-03-18 16:33:15 -0700

    fix

--- check2a.c
+++ check2a.c
@@ -40,8 +40,13 @@ int check_good2(uint32_t buflen, uint8_t padlen) {
   if (ret != buflen - padlen)
     return 1;
 
-  for (uint32_t i = 0; i < buflen - padlen; i++) {
+  for (uint32_t i = 0; i < buflen - padlen - 2; i++) {
     if (buf[i] != 0x5c) {
+      return 1;
+    }
+  }
+  for (uint32_t i = buflen - padlen - 2; i < buflen - padlen; i++) {
+    if (buf[i] != padlen) {
       return 1;
     }
   }
