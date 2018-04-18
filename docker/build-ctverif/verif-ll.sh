f10e35ad2b7973b36321efb760a33ee9dce70691
Brian Johannesmeyer <bjohanne@eng.ucsd.edu>
2018-04-18 02:18:49 -0700

    ctverif docker img and verif-ll.sh updated to take fact ll file + ctverif c wrapper

--- docker/build-ctverif/verif-ll.sh
+++ docker/build-ctverif/verif-ll.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ] || [ ! -e $1 ]; then 
-    echo "Usage: $0 llvm3.8.ll entrypoint" ; exit 1
+DOCKERIMG=bjohannesmeyer/ctverif
+
+if [ "$#" -ne 4 ] || [ ! -e $1 ] || [ ! -e $2 ] || [ ! -e $3 ]; then 
+    echo "Usage: $0 ctvwrapper.c fact.h fact-3.8.ll entrypoint" ; exit 1
 fi
-cp $1 tmp.ll
+
+CTVWRAPPERCPATH=$1
+CTVWRAPPERC=$(basename $CTVWRAPPERCPATH)
+
+FACTLIBPATH=$2
+FACTLIB=$(basename $FACTLIBPATH)
+
+FACT38LLPATH=$3
+FACTLLPATH=${FACT38LLPATH}-3.5.ll
+FACTLL=$(basename $FACTLLPATH)
+cp $FACT38LLPATH $FACTLLPATH
+
+ENTRYPOINTS=$4
 
 ## Jankily convert a 3.8 LLVM .ll file into a 3.5 LLVM .ll file
 # Find `load i32,`; replace with `load`
@@ -11,16 +25,21 @@ cp $1 tmp.ll
 # Find `smack_value* (i32*, ...) bitcast`; replace with `smack_value* (i32*, ...)* bitcast`
 # Remove lines with `!0`
 # Find `norecurse`; replace with ``
-sed -i -e 's/load[^,]*, /load /g' tmp.ll
-sed -i -e 's/getelementptr inbounds [^,]*, /getelementptr inbounds /g' tmp.ll 
-sed -i -e 's/smack_value\(.*\)) bitcast/smack_value\1)* bitcast/g' tmp.ll 
-sed -i '/!0/d' tmp.ll
-sed -i -e 's/norecurse//g' tmp.ll
+# Find `argmemonly`; replace with ``
+sed -i -e 's/load[^,]*, /load /g' $FACTLLPATH
+sed -i -e 's/getelementptr i64, /getelementptr /g' $FACTLLPATH
+sed -i -e 's/getelementptr inbounds [^,]*, /getelementptr inbounds /g' $FACTLLPATH
+sed -i -e 's/smack_value\(.*\)) bitcast/smack_value\1)* bitcast/g' $FACTLLPATH
+sed -i '/!0/d' $FACTLLPATH
+sed -i -e 's/norecurse//g' $FACTLLPATH
+sed -i -e 's/argmemonly//g' $FACTLLPATH
 
 # Copy into docker container and verify
-docker run -it -d --rm --name ctverif_cont bjohannesmeyer/ctverif /bin/bash
-docker cp tmp.ll ctverif_cont:/root/fact-verifs
-rm tmp.ll
-docker exec ctverif_cont /bin/bash -c "cd /root/fact-verifs && ENTRYPOINTS=$2 FACTLL=tmp.ll make && make clean && rm tmp.ll"
-docker stop ctverif_cont
+ID=$(docker run -it -d --rm $DOCKERIMG /bin/bash)
+trap 'docker stop $ID ; exit ' 2 # if receive SIGINT, kill the docker container
+docker cp $CTVWRAPPERCPATH $ID:/root/fact-verifs/
+docker cp $FACTLIBPATH $ID:/root/fact-verifs/
+docker cp $FACTLLPATH $ID:/root/fact-verifs/
+docker exec $ID /bin/bash -c "cd /root/fact-verifs && CTVWRAPPERC=$CTVWRAPPERC FACTLIB=$FACTLIB FACTLL=$FACTLL ENTRYPOINTS=$ENTRYPOINTS make && make clean"
+docker stop $ID
 
