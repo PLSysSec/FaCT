bd4c7e6c72d3dc2000f86d7b80fc8461c5d44132
Sunjay Cauligi <scauligi@eng.ucsd.edu>
2018-04-30 03:01:02 -0700

    saga changes

--- port/openssl/saga/micro/micro.c
+++ port/openssl/saga/micro/micro.c
@@ -6,7 +6,20 @@
 #include "inteltime.h"
 
 #define ITERATIONS 100000000
+#define MICRO_ITERATIONS 2000000000
 #define WARMUP     1000000
+
+void microc(
+    const uint8_t *out,
+    uint32_t out_len,
+    uint32_t inp_len,
+    uint8_t *data,
+    uint32_t num);
+
+uint8_t _microc(
+    uint32_t j,
+    uint32_t inp_len,
+    uint8_t out);
 
 int main() {
   uint8_t data[64];
@@ -18,6 +31,49 @@ int main() {
   for (i = 0; i < 48; i++) {
     out[i] = 1234 - i;
   }
+
+  printf("Micro:\n");
+
+  printf("using FaCT features: ");
+  fflush(stdout);
+  for (i = 0; i < WARMUP; i++) {
+    _micro1(3, 24, 83);
+  }
+  TIME_START
+  for (i = 0; i < MICRO_ITERATIONS; i++) {
+    _micro1(3, 24, 83);
+  }
+  TIME_STOP
+  TIME_DIFF(difftime);
+  printf("%lf ticks/call\n", ((double)difftime) / MICRO_ITERATIONS);
+
+  printf("C-style bitmasks: ");
+  fflush(stdout);
+  for (i = 0; i < WARMUP; i++) {
+    _micro2(3, 24, 83);
+  }
+  TIME_START
+  for (i = 0; i < MICRO_ITERATIONS; i++) {
+    _micro2(3, 24, 83);
+  }
+  TIME_STOP
+  TIME_DIFF(difftime);
+  printf("%lf ticks/call\n", ((double)difftime) / MICRO_ITERATIONS);
+
+  printf("Actually C: ");
+  fflush(stdout);
+  for (i = 0; i < WARMUP; i++) {
+    _microc(3, 9, 2);
+  }
+  TIME_START
+  for (i = 0; i < MICRO_ITERATIONS; i++) {
+    _microc(3, 9, 2);
+  }
+  TIME_STOP
+  TIME_DIFF(difftime);
+  printf("%lf ticks/call\n", ((double)difftime) / MICRO_ITERATIONS);
+
+  printf("Mini:\n");
 
   printf("using FaCT features: ");
   fflush(stdout);
@@ -40,6 +96,19 @@ int main() {
   TIME_START
   for (i = 0; i < ITERATIONS; i++) {
     micro2(out, 48, 16, data, 13);
+  }
+  TIME_STOP
+  TIME_DIFF(difftime);
+  printf("%lf ticks/call\n", ((double)difftime) / ITERATIONS);
+
+  printf("Actually C: ");
+  fflush(stdout);
+  for (i = 0; i < WARMUP; i++) {
+    microc(out, 48, 16, data, 13);
+  }
+  TIME_START
+  for (i = 0; i < ITERATIONS; i++) {
+    microc(out, 48, 16, data, 13);
   }
   TIME_STOP
   TIME_DIFF(difftime);
