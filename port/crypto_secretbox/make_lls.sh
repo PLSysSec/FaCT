301e8090fd6ad916219428bf42745a27b4cad568
scauligi <scauligi@eng.ucsd.edu>
2018-05-01 18:45:08 -0700

    moved port/ to its own repo

--- port/crypto_secretbox/make_lls.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-../../fact.byte -opt O0 -ast-out -pseudocode -generate-header -llvm-out -debug stdlib.fact crypto_poly1305.fact crypto_secretbox.fact
-cp crypto_secretbox.ll crypto_secretbox.0.ll
-../../fact.byte -opt O1 -ast-out -pseudocode -generate-header -llvm-out -debug stdlib.fact crypto_poly1305.fact crypto_secretbox.fact
-cp crypto_secretbox.ll crypto_secretbox.1.ll
-../../fact.byte -opt O2 -ast-out -pseudocode -generate-header -llvm-out -debug stdlib.fact crypto_poly1305.fact crypto_secretbox.fact
-cp crypto_secretbox.ll crypto_secretbox.2.ll
--- port/openssl/saga/structcopy.ll
+++
@@ -1,8 +0,0 @@
-
-define internal void @_structcopy(%SHA_CTX* nocapture %dst, %SHA_CTX* nocapture readonly %src) alwaysinline {
-entry:
-  %_secret_src = bitcast %SHA_CTX* %src to i8*
-  %_secret_dst = bitcast %SHA_CTX* %dst to i8*
-  tail call void @llvm.memcpy.p0i8.p0i8.i32(i8* %_secret_dst, i8* %_secret_src, i32 96, i32 4, i1 false)
-  ret void
-}
