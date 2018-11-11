338ee86f8a12bb9e82a9ebf29d38bd210bcdd419
Brian Johannesmeyer <bjohanne@eng.ucsd.edu>
2018-11-11 15:27:00 -0800

    add loop unroll bound argument to ctverif script and more error msgs

--- docker/build-ctverif/verif.sh
+++ docker/build-ctverif/verif.sh
@@ -1,27 +1,40 @@
 #!/bin/bash
 
 usage() {
-    echo "Usage: $0 [-v for verbose or -q for quiet] [entrypoint] [files ...]" 
+    echo "Usage: $0 [-v for verbose or -q for quiet] [positive integer loop unroll bound] [entrypoint] [files ...]" 
     exit 1
+}
+
+err() {
+    echo $1
+    usage
 }
 
 # Run ctverif (excluding header files from args)
 # This will probably break with any relative paths to header files that aren't in the local directory
 run_ctverif() {
-    docker exec $1 /bin/bash -c "ctverif --unroll 5 --entry-points $2 \$(find /root/verifs/ -type f ! \( -iname '*.h' \)) 2>&1"
+    docker exec $1 /bin/bash -c "ctverif --unroll $2 --entry-points $3 \$(find /root/verifs/ -type f ! \( -iname '*.h' \)) 2>&1"
 }
 
-if [ "$#" -lt 3 ]; then usage $0; fi
+# Check arg count
+if [ "$#" -lt 4 ]; then err "Not enough arguments"; fi
 
+# Get verbosity level
 if [ $1 = "-v" ]; then VERBOSE=true
 elif [ $1 = "-q" ]; then VERBOSE=false
-else usage $0; fi
+else err "Verbosity level needs to be either -v or -q"; fi
 
-ENTRYPOINT=$2
+# Get loop unroll bound and make sure its a positive integer
+UNROLL_BOUND=$2
+if ! [[ $UNROLL_BOUND =~ ^[0-9]+$ ]] ; then err "Loop unroll bound is not a positive integer"; fi
 
-for ((i=3; i<=$#; ++i)); do
+# Get entrypoint
+ENTRYPOINT=$3
+
+# Make sure input files exist
+for ((i=4; i<=$#; ++i)); do
     if [ ! -f ${!i} ]; then
-        echo "File ${!i} not found!"
+        err "File ${!i} not found"
         exit 1
     fi
 done
@@ -30,17 +43,16 @@ done
 ID=$(docker run -it -d --rm bjohannesmeyer/ctverif /bin/bash)
 
 # Copy files into container (apparently `docker cp` can't handle multiple files)
-for ((i=3; i<=$#; ++i)); do
+for ((i=4; i<=$#; ++i)); do
     docker cp ${!i} $ID:/root/verifs/
 done
-
 
 # Run ctverif and fail if "SMACK found no errors with unroll bound" is not in output
 # If verbose, print everything out as ctverif runs
 if [ $VERBOSE = true ]; then
-    OUTP=$(run_ctverif $ID $ENTRYPOINT | tee /dev/tty)
+    OUTP=$(run_ctverif $ID $UNROLL_BOUND $ENTRYPOINT | tee /dev/tty)
 else
-    OUTP=$(run_ctverif $ID $ENTRYPOINT)
+    OUTP=$(run_ctverif $ID $UNROLL_BOUND $ENTRYPOINT)
 
 fi
 
