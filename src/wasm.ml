99ed8674ee20d3ea423f62928178fd0b2aefb4b6
Garrett Gu <gu@utexas.edu>
2021-04-13 21:42:36 -0500

    refactor sprintf, add some arr support

--- src/wasm.ml
+++ src/wasm.ml
@@ -4,6 +4,7 @@ open Err
 open Tast
 open Llvm
 open Pseudocode
+open Printf
 open Codegen
 
 class local_collector m =
@@ -50,31 +51,32 @@ let collect_locals fdec =
 
 let rec build_cast pos prefix oldsize newsize is_signed wasmexpr = 
   match (oldsize, newsize, is_signed) with
-  (* TODO: cleanup this monstrocity *)
-  (* It may not be correct either... *)
+  (* TODO: cleanup this awful code *)
   | (8, 32, true) | (8, 64, true)
   | (16, 32, true) | (16, 64, true)
   | (32, 64, true) -> 
-    Printf.sprintf "(%s%d.extend%d_s %s)" prefix newsize oldsize wasmexpr
+    sprintf "(%s%d.extend%d_s %s)" prefix newsize oldsize wasmexpr
   | (8, 64, false) | (16, 64, false) | (32, 64, false) ->
-    Printf.sprintf "(%s%d.extend32_u %s)" prefix newsize wasmexpr
+    sprintf "(%s%d.extend32_u %s)" prefix newsize wasmexpr
   | (64, 32, _) -> 
-    Printf.sprintf "(%s32.wrap_%s64 %s)" prefix prefix wasmexpr
+    sprintf "(%s32.wrap_%s64 %s)" prefix prefix wasmexpr
   | (64, 16, _) | (64, 8, _) -> 
     build_cast pos prefix 32 newsize is_signed 
       (build_cast pos prefix 64 32 is_signed wasmexpr) 
     (* first cast the thing from 64 to 32 bits, then cast it down again*)
   | (32, 16, _) -> 
-    Printf.sprintf "(%s32.and (%s32.const 0xffff) %s)" prefix prefix wasmexpr
+    build_cast pos prefix 16 32 is_signed 
+      (sprintf "(%s32.and (%s32.const 0xffff) %s)" prefix prefix wasmexpr)
+    (* We may have to sign extend after truncation, so we cast it back to a 32-bit integer *)
   | (32, 8, _) | (16, 8, _) -> 
-    Printf.sprintf "(%s32.and (%s32.const 0xff) %s)" prefix prefix wasmexpr
+    build_cast pos prefix 8 32 is_signed 
+      (sprintf "(%s32.and (%s32.const 0xff) %s)" prefix prefix wasmexpr)
+    (* ditto *)
   | (8, 32, false) | (16, 32, false) 
   | (8, 8, _) | (16, 16, _) | (8, 16, false)
   | (32, 32, _) | (64, 64, _) -> wasmexpr (* no need to cast *)
   | (8, 16, true) ->
-    build_cast pos prefix 32 16 is_signed 
-      (build_cast pos prefix 8 32 is_signed wasmexpr)
-    (* first sextend to 32 bits then downsize to 16 *)
+    build_cast pos prefix 8 32 is_signed wasmexpr
   | _ -> raise @@ cerr pos "unimp cast"
 
 let append dest to_add : unit =
@@ -97,6 +99,7 @@ class wasm (m: Tast.fact_module) =
     | Bool {data=Public} 
     | UInt (_, {data=Public})
     | Int (_, {data=Public}) -> false
+    | Ref (bty, _) -> visit#is_secret bty
     | _ -> raise @@ cerr p "unimp secret"
   
   method int_bitwidth {pos=p;data} = 
@@ -105,6 +108,13 @@ class wasm (m: Tast.fact_module) =
     | UInt (w, _) -> w
     | Int (w, _) -> w
     | _ -> raise @@ cerr p "unimp bitwidth"
+  
+  method bytesize {pos=p;data} =
+    match data with
+    | Bool _ -> 1
+    | UInt (w, _) | Int (w, _) -> (w + 7) / 8
+    | Ref (bty, _) -> visit#bytesize bty
+    | _ -> raise @@ cerr p "unimp bytesize"
 
   method bty {pos=p;data} = 
     match data with
@@ -119,7 +129,13 @@ class wasm (m: Tast.fact_module) =
     | UInt (64, {data=Secret}) | Int (64, {data=Secret}) -> "s64"
     | UInt (64, {data=Public}) | Int (64, {data=Public}) -> "i64"
     | Ref (bty, _) -> visit#bty bty
+    | Arr (bty, _, _) -> "i32"
     | _ -> raise @@ cerr p "unimp bty"
+  
+  method arr_bty {pos=p;data} = 
+    match data with
+    | Arr (bty, _, _) -> bty
+    | _ -> raise @@ cerr p "expected array type"
 
   method param {pos=p;data} = 
     match data with
@@ -144,48 +160,60 @@ class wasm (m: Tast.fact_module) =
   method binop p op is_signed we1 we2 bty =
     let sign = (if is_signed then "s" else "u") in
       match op with 
-      | Ast.Plus -> Printf.sprintf "(%s.add %s %s)" bty we1 we2
-      | Ast.Minus -> Printf.sprintf "(%s.sub %s %s)" bty we1 we2
-      | Ast.Multiply -> Printf.sprintf "(%s.mul %s %s)" bty we1 we2
+      | Ast.Plus -> sprintf "(%s.add %s %s)" bty we1 we2
+      | Ast.Minus -> sprintf "(%s.sub %s %s)" bty we1 we2
+      | Ast.Multiply -> sprintf "(%s.mul %s %s)" bty we1 we2
       | Ast.Divide -> 
-        Printf.sprintf "(%s.div_%s %s %s)" bty sign we1 we2
+        sprintf "(%s.div_%s %s %s)" bty sign we1 we2
       | Ast.Modulo -> 
-        if is_signed then Printf.sprintf "(%s.rem_s %s %s)" bty we1 we2
-        else Printf.sprintf "(%s.rem_u %s %s)" bty we1 we2
-      | Ast.Equal -> Printf.sprintf "(%s.eq %s %s)" bty we1 we2
-      | Ast.NEqual -> Printf.sprintf "(%s.ne %s %s)" bty we1 we2
+        if is_signed then sprintf "(%s.rem_s %s %s)" bty we1 we2
+        else sprintf "(%s.rem_u %s %s)" bty we1 we2
+      | Ast.Equal -> sprintf "(%s.eq %s %s)" bty we1 we2
+      | Ast.NEqual -> sprintf "(%s.ne %s %s)" bty we1 we2
       | Ast.GT -> 
-        Printf.sprintf "(%s.gt_%s %s %s)" bty sign we1 we2
+        sprintf "(%s.gt_%s %s %s)" bty sign we1 we2
       | Ast.GTE -> 
-        Printf.sprintf "(%s.ge_%s %s %s)" bty sign we1 we2
+        sprintf "(%s.ge_%s %s %s)" bty sign we1 we2
       | Ast.LT -> 
-        Printf.sprintf "(%s.lt_%s %s %s)" bty sign we1 we2
+        sprintf "(%s.lt_%s %s %s)" bty sign we1 we2
       | Ast.LTE -> 
-        Printf.sprintf "(%s.le_%s %s %s)" bty sign we1 we2
-      | Ast.LogicalAnd | Ast.BitwiseAnd -> Printf.sprintf "(%s.and %s %s)" bty we1 we2
-      | Ast.LogicalOr | Ast.BitwiseOr -> Printf.sprintf "(%s.or %s %s)" bty we1 we2
-      | Ast.BitwiseXor -> Printf.sprintf "(%s.xor %s %s)" bty we1 we2
-      | Ast.LeftShift -> Printf.sprintf "(%s.shl %s %s)" bty we1 we2
-      | Ast.RightShift -> Printf.sprintf "(%s.shr_%s %s %s)" bty sign we1 we2
-      | Ast.LeftRotate -> Printf.sprintf "(%s.rotl %s %s)" bty we1 we2
-      | Ast.RightRotate -> Printf.sprintf "(%s.rotr %s %s)" bty we1 we2
+        sprintf "(%s.le_%s %s %s)" bty sign we1 we2
+      | Ast.LogicalAnd | Ast.BitwiseAnd -> sprintf "(%s.and %s %s)" bty we1 we2
+      | Ast.LogicalOr | Ast.BitwiseOr -> sprintf "(%s.or %s %s)" bty we1 we2
+      | Ast.BitwiseXor -> sprintf "(%s.xor %s %s)" bty we1 we2
+      | Ast.LeftShift -> sprintf "(%s.shl %s %s)" bty we1 we2
+      | Ast.RightShift -> sprintf "(%s.shr_%s %s %s)" bty sign we1 we2
+      | Ast.LeftRotate -> sprintf "(%s.rotl %s %s)" bty we1 we2
+      | Ast.RightRotate -> sprintf "(%s.rotr %s %s)" bty we1 we2
   
   method unop p op wexpr wbty =
     match op with
     | Ast.LogicalNot -> 
-      Printf.sprintf "(%s.xor %s (%s.const 1))" wbty wexpr wbty
+      sprintf "(%s.xor %s (%s.const 1))" wbty wexpr wbty
     | Ast.BitwiseNot -> 
-      Printf.sprintf "(%s.xor %s (%s.const -1))" wbty wexpr wbty
+      sprintf "(%s.xor %s (%s.const -1))" wbty wexpr wbty
     | Ast.Neg -> 
-      Printf.sprintf "(%s.sub (%s.const 0) %s)" wbty wbty wexpr
+      sprintf "(%s.sub (%s.const 0) %s)" wbty wbty wexpr
+    
+  method lexpr {pos=p;data} = 
+    match data with
+    | LIntLiteral n -> sprintf "(i64.const %d)" n
+    | LDynamic x -> sprintf "(get_local $%s)" x.data
+
+  method addr_of arr lexpr =
+    let (_, arr_ty) = arr in
+    let arr_ty = visit#arr_bty arr_ty in (* get the underlying type of the array *)
+    let elem_sz = visit#bytesize arr_ty in
+    sprintf "(i32.add %s (i32.mul (i32.const %d) (i32.wrap_i64 %s)))"
+    (visit#expr arr) elem_sz (visit#lexpr lexpr)
 
   method expr ({pos=p; data}, bty) =
     let wbty = visit#bty bty in
     match data with
       | True -> "(" ^ wbty ^ ".const 1)"
       | False -> "(" ^ wbty ^ ".const 0)"
-      | IntLiteral n -> Printf.sprintf "(%s.const %d)" wbty n
-      | Variable x -> Printf.sprintf "(get_local %s)" (visit#varname x)
+      | IntLiteral n -> sprintf "(%s.const %d)" wbty n
+      | Variable x -> sprintf "(get_local %s)" (visit#varname x)
       | Enref e -> visit#expr e
       | Deref e -> visit#expr e
       | UnOp (op, e) -> 
@@ -196,15 +224,15 @@ class wasm (m: Tast.fact_module) =
         let e1ty = Tast_util.type_of e1 in
         let is_signed = Tast_util.(not (is_bool e1ty) && is_signed e1ty) in
           visit#binop p op is_signed we1 we2 wbty
-      | Classify e -> Printf.sprintf "(%s.classify %s)" wbty (visit#expr e)
-      | Declassify e -> Printf.sprintf "(%s.declassify %s)" wbty (visit#expr e)
+      | Classify e -> sprintf "(%s.classify %s)" wbty (visit#expr e)
+      | Declassify e -> sprintf "(%s.declassify %s)" wbty (visit#expr e)
       | TernOp (cond, e1, e2) | Select (cond, e1, e2) -> 
         let wcond = visit#expr cond in
         let we1 = visit#expr e1 in
         let we2 = visit#expr e2 in
         let (_, cond_bty) = cond in
         let op = (if (visit#is_secret cond_bty) then "sselect" else "select") in
-        Printf.sprintf "(%s %s %s %s)" op we1 we2 wcond
+        sprintf "(%s %s %s %s)" op we1 we2 wcond
       | Cast (castty, e) -> 
         let we = visit#expr e in
         let (_, oldbty) = e in
@@ -213,7 +241,15 @@ class wasm (m: Tast.fact_module) =
         let is_signed = Tast_util.is_signed oldbty in
         let prefix = if (visit#is_secret castty) then "s" else "i" in
         build_cast p prefix oldsize newsize is_signed we
-      | _ -> raise @@ cerr p "unimp expr"
+      | ArrayGet (e, lexpr) -> 
+        let (_, arr_ty) = e in
+        let arr_ty = visit#arr_bty arr_ty in (* get the underlying type of the array *)
+        let mem = if (visit#is_secret arr_ty) then "0" else "1" in
+        sprintf "(%s.load %s %s)"
+          (visit#bty arr_ty) mem (visit#addr_of e lexpr)
+      | ArrayView (e, lexpr, _) -> (* we don't care about the length *)
+        visit#addr_of e lexpr
+      | _ -> sprintf "(unimp %s)" (show_expr' data)
 
   method assignment p dest wexpr = 
     match dest with
@@ -233,7 +269,7 @@ class wasm (m: Tast.fact_module) =
           | ({data=Variable v}, bty) -> 
             let (_, cond_bty) = cond in
             let op = (if (visit#is_secret cond_bty) then "sselect" else "select") in
-            Printf.sprintf "(set_local %s (%s %s (get_local %s) %s))" 
+            sprintf "(set_local %s (%s %s (get_local %s) %s))" 
             (visit#varname v) op (visit#expr e2) (visit#varname v) (visit#expr cond)
             (* If cond then select e2, otherwise select e1, 
               then move the resulting value back into e1 *)
@@ -242,12 +278,12 @@ class wasm (m: Tast.fact_module) =
       | FnCall (x, bty, fn, args) ->
         let wargs = List.map visit#expr args |> String.concat " " in
         let fname = fn.data in
-        Printf.sprintf "(set_local %s (call $%s %s))" 
+        sprintf "(set_local %s (call $%s %s))" 
           (visit#varname x) fname wargs
       | VoidFnCall (fn, args) ->
         let wargs = List.map visit#expr args |> String.concat " " in
         let fname = fn.data in
-        Printf.sprintf "(call $%s %s)" fname wargs
+        sprintf "(call $%s %s)" fname wargs
       | Assume e -> ""
 
   method block ({pos=p; data}, next) = 
@@ -259,12 +295,12 @@ class wasm (m: Tast.fact_module) =
         | ListOfStuff stms -> 
           append res((List.map visit#stm stms) |> String.concat " ");
         | If (cond,thens,elses) ->
-          append res (Printf.sprintf "(if %s (%s) (%s))" 
+          append res (sprintf "(if %s (%s) (%s))" 
             (visit#expr cond)
             (visit#block thens)
             (visit#block elses)
           )
-        | _ -> raise @@ cerr p "unimp block"
+        | _ -> append res (sprintf "(unimp %s)" (show_block' data))
     end;
     append res (visit#next next);
     res.contents
@@ -274,10 +310,10 @@ class wasm (m: Tast.fact_module) =
       | Block blk -> 
         visit#block blk
       | End -> ""
-      | Return e -> Printf.sprintf "(return %s)" (visit#expr e)
+      | Return e -> sprintf "(return %s)" (visit#expr e)
       | VoidReturn -> "(return)"
 
-  method fdec ({pos=p; data} as fdec: Tast.function_dec) : string = 
+  method fdec ({pos=p; data} as fdec) : string = 
     match data with
       | FunDec(name, fnattr, rt, params, body) -> 
         let res = ref "" in
@@ -293,6 +329,8 @@ class wasm (m: Tast.fact_module) =
 
         append res ")";
         res.contents
+      | StdlibFn(code, fnattr, rt, params) -> 
+        sprintf "(unimp %s)" (show_stdlib_code code)
       | _ -> raise @@ cerr p "unimp fdec"
   
   method fact_module () = 
@@ -300,6 +338,15 @@ class wasm (m: Tast.fact_module) =
     append res "(module ";
     let Module (sdecs,fdecs,minfo) = m in
     let _ = List.map visit#sdec sdecs in
+
+    let memsz = 512 in
+    let pagesz = 65536 in
+
+    append res (sprintf "(import \"js\" \"memory\" (memory $secmem secret %d)) " memsz);
+    append res (sprintf "(import \"js\" \"memory\" (memory $pubmem %d)) " memsz);
+
+    append res (sprintf "(global $srsp (mut i32) (i32.const %d)) " (memsz * pagesz));
+    append res (sprintf "(global $prsp (mut i32) (i32.const %d)) " (memsz * pagesz));
     
     append res (List.map visit#fdec (List.rev fdecs) |> String.concat " ");
 
